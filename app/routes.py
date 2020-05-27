a27731a580966d39606b42e257cbbae27bf0c73e
Bibiana Barbosa <balbuenabibi@gmail.com>
2020-05-27 20:25:47 +0100

    base templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Bibi'}
+    posts = [
+        {
+            'author': {'username': 'Cat'},
+            'body': 'Need help with flask!'
+        },
+        {
+            'author': {'username': 'Bob'},
+            'body': 'How do I use Azure API?'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
