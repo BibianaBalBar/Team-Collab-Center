3396eed5bf69ba23fb2b6a2bc806573b9997ddd0
Bibiana Barbosa <balbuenabibi@gmail.com>
2020-05-29 23:06:17 +0100

    pagination

--- app/routes.py
+++ app/routes.py
@@ -15,19 +15,16 @@ def before_request():
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
-def index():
-    # form = PostForm()
-    posts = current_user.team_posts()
-    # if form.validate_on_submit():
-    #     post = Post(title=form.title.data, 
-    #                 description=form.post.data, 
-    #                 author=current_user,
-    #                 team_id=current_user.team_id)
-    #     db.session.add(post)
-    #     db.session.commit()
-    #     flash('Your post is now live!')
-    #     return redirect(url_for('index'))
-    return render_template('index.html', title='Home', posts=posts)
+def index():  
+    page = request.args.get('page', 1, type=int)  
+    posts = current_user.team_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None    
+    return render_template('index.html', title='Home', posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 @app.route('/create_issue', methods=['GET', 'POST'])
 @login_required
@@ -47,8 +44,15 @@ def create_issue():
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items,
+                          next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -93,8 +97,16 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = Post.query.filter_by(user_id=current_user.id)   
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.filter_by(user_id=current_user.id).order_by(
+        Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
